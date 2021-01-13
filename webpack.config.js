4f66bac87bf05abc7a693d996d0ce20ed1455da8
daniilorain <dnlorn@gmail.com>
2021-01-13 22:10:31 +0300

    add graphqli-explorer

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+  mode: mode,
+  entry: {
+    'graphqli-explorer-page': './src/graphqli-explorer-page.jsx'
+  },
+  output: {
+    filename: './build/[name].js',
+    path: path.resolve('docs')
+  },
+  resolve: {
+    extensions: ['.mjs', '.jsx', '.js', '.css']
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.jsx?$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+};
